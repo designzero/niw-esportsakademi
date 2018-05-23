bf730082931fd9d55e9d946838bbe276a297dd97
Björn Johansson [bjojoh17] <bjojoh17@iteds-12920.hh.se>
2018-05-23 13:08:30 +0200

    add/update quiz html, update css, js

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,22 @@ $(document).ready(function() {
 
 	});
 });
+
+$("#examine").click(function () {
+	
+}
+
+
+$("#check-answers").click(function () {
+	$("input+label").removeClass("bg-wrong");
+	$("input+label").removeClass("bg-correct");
+
+	$("input:checked").not(".correct").next().addClass("bg-wrong");
+	$(".correct:input:checked+label").addClass("bg-correct");
+});
+
+// function checkAnswers() {
+// 	if ("input:checked").hasClass("correct") {
+
+// 	}
+// };
